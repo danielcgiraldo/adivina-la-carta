a71cd2917e6a83c5da7b412427a4ccced4ec7f07
Daniel Castillo Giraldo <dc.giraldocg@hotmail.com>
2022-04-06 14:58:03 -0500

    Update canva.py

--- canva.py
+++ canva.py
@@ -1,6 +1,5 @@
 from tkinter import *
 import time
-import tkinter
 
 root = Tk()
 
@@ -33,7 +32,7 @@ def start_game(event):
     l(item2)
     l_m(item3)
 
-photo1 = tkinter.PhotoImage(file="card.png")
+photo1 = PhotoImage(file="./resources/images/card.png")
 width1 = photo1.width()
 height1 = photo1.height()
 canva = Canvas(width=width1*3 + 70, height=height1 + 50)
