3bf945f847631778e9f09ee8187dff66c1082e30
Daniel Castillo Giraldo <dc.giraldocg@hotmail.com>
2022-04-05 22:57:53 -0500

    Sorting Cards

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # fundamentos-proyecto
  Proyecto de Programación Python para la materia Fundamentos de Programación
+
+
+# Referencias
+
+- https://gist.github.com/stahnni/3b1effd6e4a43d25a57575bcec2cfbbe
+- https://stackoverflow.com/questions/2679418/how-to-get-the-coordinates-of-an-object-in-a-tkinter-canvas
+- https://stackoverflow.com/questions/23275445/move-an-image-in-python-using-tkinter
+- https://docs.python.org/3/library/tk.html
---
+++ canva.py
@@ -0,0 +1,47 @@
+from tkinter import *
+import time
+
+root = Tk()
+
+def l_m(item):
+    canva.move(item, 100, 0)
+    root.update()
+    time.sleep(0.05)
+    if(canva.coords(item)[0] < 3*x + 15):
+        canva.after(10, l_m(item))
+        root.update()
+
+def l(item):
+    canva.move(item, -100, 0)
+    root.update()
+    time.sleep(0.05)
+    if(canva.coords(item)[0] > x + 30):
+        canva.after(10, l(item))
+
+def r(item):
+    canva.move(item, 100, 0)
+    root.update()
+    time.sleep(0.05)
+    if(canva.coords(item)[0] < 5*x + 30):
+        canva.after(10, r(item))
+
+
+def start_game(event):
+    r(item1)
+    l(item3)
+    l(item2)
+    l_m(item3)
+
+photo1 = PhotoImage(file=r"resources/images/card.png")
+width1 = photo1.width()
+height1 = photo1.height()
+canva = Canvas(width=width1*3 + 70, height=height1 + 50)
+canva.pack(expand=1, fill=BOTH) # <--- Make your canvas expandable.
+x = (width1)/2.0
+y = (height1)/2.0
+item1 = canva.create_image(x + 30, y, image=photo1)
+item2 = canva.create_image(3*x + 30, y, image=photo1)
+item3 = canva.create_image(5*x + 30, y, image=photo1)
+
+canva.bind('<Button-1>', start_game)
+root.mainloop() 
